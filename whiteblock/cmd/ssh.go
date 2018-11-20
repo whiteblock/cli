72b2749575a1f825fbfc4c247cebd6763c3f1fa5
Daniel Choi <daniel@Daniels-MacBook-Pro.local>
2018-11-20 14:56:30 -0800

    fix ssh

--- whiteblock/cmd/ssh.go
+++ whiteblock/cmd/ssh.go
@@ -2,18 +2,13 @@ package cmd
 
 import (
 	"fmt"
+	"os/exec"
 
 	"github.com/spf13/cobra"
 )
 
-var (
-	command   string
-	node      int
-	sshserver string
-)
-
 var sshCmd = &cobra.Command{
-	Use:   "ssh",
+	Use:   "ssh <node> <server> <command> ",
 	Short: "SSH into an existing container.",
 	Long: `
 SSH will allow the user to go into the contianer where the specified node exists.
@@ -24,7 +19,18 @@ Response: stdout of the command
 	Run: func(cmd *cobra.Command, args []string) {
 		//add websocket command later
 		serverAddr = "ws://" + serverAddr + "/socket.io/?EIO=3&transport=websocket"
-		msg := "{\"server\":" + fmt.Sprintf("%s", sshserver) + ",\"node\":" + fmt.Sprintf("%d", node) + ",\"command\":\"" + command + "\"}"
+		// msg := "{\"server\":" + fmt.Sprintf("%s", sshserver) + ",\"node\":" + fmt.Sprintf("%d", node) + ",\"command\":\"" + command + "\"}"
+
+		if len(args) < 3 {
+			println("Invalid number of arguments given")
+			out, err := exec.Command("bash", "-c", "./whiteblock ssh -h").Output()
+			if err != nil {
+				panic(err)
+			}
+			fmt.Printf("%s", out)
+		}
+
+		msg := "{\"server\":" + args[0] + ",\"node\":" + args[1] + ",\"command\":\"" + args[2] + "\"}"
 
 		wsSSH(serverAddr, msg)
 	},
@@ -32,8 +38,6 @@ Response: stdout of the command
 
 func init() {
 	sshCmd.Flags().StringVarP(&serverAddr, "server-addr", "a", "localhost:5000", "server address with port 5000")
-	sshCmd.Flags().StringVarP(&command, "cmd", "c", "ls -l", "Which shell to run in container")
-	sshCmd.Flags().StringVarP(&sshserver, "server", "s", "1", "Which server to run in")
-	sshCmd.Flags().IntVarP(&node, "node", "n", 0, "Node number to SSH into")
+
 	RootCmd.AddCommand(sshCmd)
 }
