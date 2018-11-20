818e030d4540c48f4e0f52511ee4537b5b994cf6
Daniel Choi <daniel@Daniels-MacBook-Pro.local>
2018-11-20 14:16:22 -0800

    add to netconfig

--- whiteblock/cmd/netconfig.go
+++ whiteblock/cmd/netconfig.go
@@ -12,27 +12,90 @@ var (
 )
 
 var netropyCmd = &cobra.Command{
-	Use:     "netconfig <engine number> <path number> <command>",
+	Use:     "netconfig <engine number> <path number> <engine number> <path number> <command>",
 	Aliases: []string{"emulate"},
 	Short:   "Network conditions",
-	Long: `Netconfig will introduce persisting network conditions for testing.
+	Long: `
+Netconfig will introduce persisting network conditions for testing.
 	
 	latency 			Specifies the latency to add [ms];
 	packetloss 			Specifies the amount of packet loss to add [%];
+	bandwidth			Specifies the bandwidth of the network [bps|Kbps|Mbps|Gbps];
 	`,
 
 	Run: func(cmd *cobra.Command, args []string) {
 		serverAddr = "ws://" + serverAddr + "/socket.io/?EIO=3&transport=websocket"
 		command := "netconfig"
-		msg := "engine " + args[0] + " path " + args[1] + " " + strings.Join(args[2:], " ")
+		msg := "engine " + args[0] + " path " + args[1] + " " + strings.Join(args[4:], " ")
 
 		wsSendCmd(serverAddr, command, msg)
+	},
+}
+
+var latencyCmd = &cobra.Command{
+	Use:     "latency <engine number> <path number> <engine number> <path number>",
+	Aliases: []string{"lat"},
+	Short:   "Latency",
+	Long: `
+Latency will introduce delay to the network.
+	`,
+
+	Run: func(cmd *cobra.Command, args []string) {
+		serverAddr = "ws://" + serverAddr + "/socket.io/?EIO=3&transport=websocket"
+		command := "netconfig"
+		msg1 := "engine " + args[0] + " path " + args[1] + " set delay constant port 1 to port 2"
+		msg2 := "engine " + args[2] + " path " + args[3] + " set delay constant port 2 to port 1"
+
+		wsSendCmd(serverAddr, command, msg1)
+		wsSendCmd(serverAddr, command, msg2)
+	},
+}
+
+var packetLossCmd = &cobra.Command{
+	Use:     "packetloss <engine number> <path number> <engine number> <path number> <percent>",
+	Aliases: []string{"loss"},
+	Short:   "Packetloss",
+	Long: `
+Packetloss will drop packets in the network.
+	`,
+	Run: func(cmd *cobra.Command, args []string) {
+		serverAddr = "ws://" + serverAddr + "/socket.io/?EIO=3&transport=websocket"
+		command := "netconfig"
+		msg1 := "engine " + args[0] + " path " + args[1] + " set loss random " + args[4] + " port 1 to port 2"
+		msg2 := "engine " + args[2] + " path " + args[3] + " set loss random " + args[4] + " port 2 to port 1"
+
+		wsSendCmd(serverAddr, command, msg1)
+		wsSendCmd(serverAddr, command, msg2)
+	},
+}
+
+var bandwCmd = &cobra.Command{
+	Use:     "bandwidth <engine number> <path number> <engine number> <path number> <bandwidth number> <bandwidth type>",
+	Aliases: []string{"bw"},
+	Short:   "Bandwidth",
+	Long: `
+Bandwidth will constrict the network to the specified bandwidth.
+
+Fomat: 
+	bandwidth type: bps, Kbps, Mbps, Gbps
+	`,
+
+	Run: func(cmd *cobra.Command, args []string) {
+		serverAddr = "ws://" + serverAddr + "/socket.io/?EIO=3&transport=websocket"
+		command := "netconfig"
+		msg1 := "engine " + args[0] + " path " + args[1] + " set bw fixed " + args[4] + args[5]
+		msg2 := "engine " + args[2] + " path " + args[3] + " set bw fixed " + args[4] + args[5]
+
+		wsSendCmd(serverAddr, command, msg1)
+		wsSendCmd(serverAddr, command, msg2)
 	},
 }
 
 func init() {
 	netropyCmd.Flags().IntVarP(&latency, "latency", "l", 10, "latency")
 	netropyCmd.Flags().Float32VarP(&packetloss, "packetloss", "p", 0.001, "packetloss")
+
+	netropyCmd.AddCommand(latencyCmd, packetLossCmd, bandwCmd)
 
 	RootCmd.AddCommand(netropyCmd)
 }
