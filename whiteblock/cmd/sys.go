7e479c34ccb268f0a744fa33032f2da7a69e12c3
Daniel Choi <daniel@Daniels-MacBook-Pro.local>
2018-12-13 14:35:59 -0800

    stupid

--- whiteblock/cmd/sys.go
+++ whiteblock/cmd/sys.go
@@ -1,12 +1,14 @@
 package cmd
 
 import (
+	"bufio"
 	"encoding/json"
 	"fmt"
 	"os"
 	"os/exec"
 	"reflect"
 	"strconv"
+	"strings"
 
 	"github.com/spf13/cobra"
 )
@@ -153,7 +155,98 @@ Params: Test number
 		y := 500
 		z := 0
 
-		buildCmd.Execute()
+		buildCmd.Run = func(cmd *cobra.Command, args []string) {
+			buildArr := make([]string, 0)
+			paramArr := make([]string, 0)
+			serverAddr = "ws://" + serverAddr + "/socket.io/?EIO=3&transport=websocket"
+			bldcommand := "build"
+
+			buildOpt := [6]string{"servers (required server number)", "blockchain (default set to: ethereum)", "nodes (default set to: 10)", "image (default set to: ethereum:latest)", "cpus (default set to: no limit)", "memory (default set to: no limit)"}
+			defOpt := [6]string{"[]", "ethereum", "10", "ethereum:latest", "", ""}
+
+			scanner := bufio.NewScanner(os.Stdin)
+			for i := 0; i < len(buildOpt); i++ {
+				fmt.Print(buildOpt[i] + ": ")
+				scanner.Scan()
+
+				text := scanner.Text()
+				if len(text) != 0 {
+					buildArr = append(buildArr, text)
+				} else {
+					buildArr = append(buildArr, defOpt[i])
+				}
+			}
+
+			if buildArr[0] == "[]" {
+				println("Invalid server. Please specify a server; none was given.")
+				os.Exit(2)
+			}
+
+			server := "[" + buildArr[0] + "]"
+			blockchain := buildArr[1]
+			nodes := buildArr[2]
+			image := buildArr[3]
+			cpu := buildArr[4]
+			memory := buildArr[5]
+
+			fmt.Print("Use default parameters? (y/n) ")
+			scanner.Scan()
+			ask := scanner.Text()
+
+			if ask == "y" {
+			} else {
+				getParamCommand := "get_params"
+				bcparam := []byte(wsEmitListen(serverAddr, getParamCommand, blockchain))
+				var paramlist []map[string]string
+
+				json.Unmarshal(bcparam, &paramlist)
+
+				scanner := bufio.NewScanner(os.Stdin)
+
+				for i := 0; i < len(paramlist); i++ {
+					for key, value := range paramlist[i] {
+						fmt.Print(key, " ("+value+"): ")
+						scanner.Scan()
+						text := scanner.Text()
+						if value == "string" {
+							if len(text) != 0 {
+								if fmt.Sprint(reflect.TypeOf(text)) != "string" {
+									println("bad type")
+									os.Exit(2)
+								}
+								paramArr = append(paramArr, "\""+key+"\""+": "+"\""+text+"\"")
+							} else {
+								continue
+							}
+						} else if value == "[]string" {
+							if len(text) != 0 {
+								tmp := strings.Replace(text, " ", ",", -1)
+								paramArr = append(paramArr, "\""+key+"\""+": "+"["+tmp+"]")
+							} else {
+								continue
+							}
+						} else if value == "int" {
+							if len(text) != 0 {
+								_, err := strconv.Atoi(text)
+								if err != nil {
+									println("bad type")
+									os.Exit(2)
+								}
+								paramArr = append(paramArr, "\""+key+"\""+": "+text)
+							} else {
+								continue
+							}
+						}
+					}
+				}
+			}
+
+			param := "{\"servers\":" + fmt.Sprintf("%s", server) + ",\"blockchain\":\"" + blockchain + "\",\"nodes\":" + nodes + ",\"image\":\"" + image + "\",\"resources\":{\"cpus\":\"" + cpu + "\",\"memory\":\"" + memory + "\"},\"params\":{" + strings.Join(paramArr[:], ",") + "}}"
+			stat := wsEmitListen(serverAddr, bldcommand, param)
+			if stat == "" {
+				writeFile(param)
+			}
+		}
 
 		pinArgs = append(pinArgs, args[0])
 		pinArgs = append(pinArgs, args[1])
