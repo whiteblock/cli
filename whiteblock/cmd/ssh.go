9971fc69322ec275185d41d61ddda44a812daabe
Nathaniel Blakely <nate@whiteblock.io>
2018-12-19 18:10:34 +0000

    ssh.go edited online with Bitbucket

--- whiteblock/cmd/ssh.go
+++ whiteblock/cmd/ssh.go
@@ -57,7 +57,7 @@ Response: stdout of the command
 		param := "{\"server\":" + args[0] + ",\"node\":" + args[1] + ",\"command\":\"service ssh start\"}"
 		wsEmitListen(serverAddr, command2, param)
 
-		err = unix.Exec("/usr/bin/ssh", []string{"ssh", "root@" + fmt.Sprintf(node[nodeNumber].IP)}, os.Environ())
+		err = unix.Exec("/usr/bin/ssh", []string{"ssh","-o","StrictHostKeyChecking no", "root@" + fmt.Sprintf(node[nodeNumber].IP)}, os.Environ())
 		log.Fatal(err)
 	},
 }
