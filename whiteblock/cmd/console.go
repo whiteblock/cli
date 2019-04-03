472f2f2ab453b3af38852736d0650b951deb7329
Daniel <choidanielw@gmail.com>
2019-04-03 16:55:45 -0700

    add a common use tmux attach console session

---
+++ whiteblock/cmd/console.go
@@ -0,0 +1,44 @@
+package cmd
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+
+	util "../util"
+	"github.com/spf13/cobra"
+	"golang.org/x/sys/unix"
+)
+
+var console = &cobra.Command{
+	Use:   "console <node>",
+	Short: "Logs into the geth console",
+	Long: `
+Console will log into the geth console.
+
+Response: stdout of geth console`,
+	Run: func(cmd *cobra.Command, args []string) {
+		util.CheckArguments(cmd, args, 1, 1)
+		nodes, err := GetNodes()
+		if err != nil {
+			util.PrintErrorFatal(err)
+		}
+
+		nodeNumber, err := strconv.Atoi(args[0])
+		if err != nil {
+			util.PrintErrorFatal(err)
+		}
+		if nodeNumber >= len(nodes) {
+			util.PrintStringError("Node number too high")
+			os.Exit(1)
+		}
+		log.Fatal(unix.Exec("/usr/bin/ssh", []string{"ssh", "-i", "/home/master-secrets/id.master", "-o", "StrictHostKeyChecking no",
+		"-o", "UserKnownHostsFile=/dev/null", "-o", "PasswordAuthentication no", "-o", "ConnectTimeout=10", "-y", "-t",
+		"root@" + fmt.Sprintf(nodes[nodeNumber].IP), "tmux", "attach", "-t", "whiteblock"}, os.Environ()))
+	},
+}
+
+func init() {
+	RootCmd.AddCommand(console)
+}
