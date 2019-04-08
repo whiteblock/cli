a086d81a66f6293d59ab9596a99ed09ed1d4edee
Daniel <choidanielw@gmail.com>
2019-04-08 12:38:03 -0700

    add brackets to no

--- whiteblock/util/input.go
+++ whiteblock/util/input.go
@@ -11,7 +11,7 @@ func YesNoPrompt(msg string) bool {
 	scanner := bufio.NewScanner(os.Stdin)
 
 	for {
-		fmt.Printf("%s ([y]es/no) ", msg)
+		fmt.Printf("%s ([y]es/[n]o) ", msg)
 		scanner.Scan()
 		ask := scanner.Text()
 		ask = strings.Trim(ask, "\n\t\r\v\f ")
