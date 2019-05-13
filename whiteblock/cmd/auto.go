7686d02f0e0028405f6fc6a1f760f5d6aa405ad2
n8wb <vanish172@gmail.com>
2019-05-13 14:51:49 +0000

    add ability to specify constant params

--- whiteblock/cmd/auto.go
+++ whiteblock/cmd/auto.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	util "../util"
+	"encoding/json"
 	"fmt"
 	"github.com/spf13/cobra"
 	"strconv"
@@ -9,13 +10,13 @@ import (
 
 var autoCmd = &cobra.Command{
 	Aliases: []string{},
-	Use:     "auto <node> <command>",
+	Use:     "auto <node> <command> [params]",
 	Short:   "send queries",
 	Long: `Automatically send queries to a node in the background. <command> is the name of the json rpc call to be made
 `,
 
 	Run: func(cmd *cobra.Command, args []string) {
-		util.CheckArguments(cmd, args, 2, 2)
+		util.CheckArguments(cmd, args, 2, -1)
 		node, err := strconv.Atoi(args[0])
 		if err != nil {
 			util.PrintErrorFatal(err)
@@ -28,12 +29,24 @@ var autoCmd = &cobra.Command{
 		if err != nil {
 			util.PrintErrorFatal(err)
 		}
+		params := []interface{}{}
+		if len(args) > 2 {
+			for _, arg := range args[2:] {
+				var param interface{}
+				err = json.Unmarshal([]byte(arg), &param)
+				if err != nil {
+					param = arg //if it is not json, then it is a string
+				}
+				params = append(params, param)
+			}
+		}
 		jsonRpcCallAndPrint("setup_load", []interface{}{map[string]interface{}{
 			"node":            node,
 			"name":            fmt.Sprintf("node%d:%s", node, args[1]),
 			"interval":        interval,
 			"sendPerInterval": sendPerInterval,
 			"call":            args[1],
+			"arguments":       params,
 		}})
 	},
 }
