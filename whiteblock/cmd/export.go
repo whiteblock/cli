c29b62a5c06c8eb18b1a6bff4edf21ff31b47494
n8wb <vanish172@gmail.com>
2019-07-07 08:26:01 +0800

    add single node mode

--- whiteblock/cmd/export.go
+++ whiteblock/cmd/export.go
@@ -253,10 +253,14 @@ var exportCmd = &cobra.Command{
 		if err != nil {
 			util.PrintErrorFatal(err)
 		}
+		singleNodeMode, err := cmd.Flags().GetBool("single-node-mode")
+		if err != nil {
+			util.PrintErrorFatal(err)
+		}
 
 		os.MkdirAll(outputDir, 0755)
 		if local {
-			fetchDataLocally(outputDir, startBlock)
+			fetchDataLocally(outputDir, startBlock, singleNodeMode)
 			return
 		}
 
@@ -430,11 +434,17 @@ func fetchBlockDataLocally(sem *semaphore.Weighted, node Node, blockHeight int, 
 }
 
 //only supports the main log and the block data
-func fetchDataLocally(dir string, startBlock int) {
+func fetchDataLocally(dir string, startBlock int, singleNodeMode bool) {
 	sem := semaphore.NewWeighted(conf.MaxConns)
 	nodes, err := GetNodes()
 	if err != nil {
 		util.PrintErrorFatal(err)
+	}
+	if len(nodes) < 1 {
+		return
+	}
+	if singleNodeMode {
+		nodes = nodes[:1]
 	}
 	blockHeights := make([]int, len(nodes))
 	for i := range nodes {
@@ -482,6 +492,7 @@ func fetchDataLocally(dir string, startBlock int) {
 func init() {
 	exportCmd.Flags().Bool("local", false, "get data from the local nodes instead of the API")
 	exportCmd.Flags().String("dir", ".", "specify a custom output directory")
-	exportCmd.Flags().Int("start-block", 1, "the export start time for local only")
+	exportCmd.Flags().Int("start-block", 1, "the export start block for local only")
+	exportCmd.Flags().Bool("single-node-mode", 1, "the export start time for local only")
 	RootCmd.AddCommand(exportCmd)
 }
