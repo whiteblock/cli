c6021d446a0b4b5f79dc66047da08a9558326e16
renaynay <renenayman11@gmail.com>
2019-07-30 19:12:24 -0400

    test for Prettypi

--- whiteblock/util/prettyp_test.go
+++ whiteblock/util/prettyp_test.go
@@ -34,4 +34,32 @@ func TestPrettyp(t *testing.T) {
 	}
 }
 
+func TestPrettypi(t *testing.T) {
+	var tests = []struct {
+		i interface{}
+		expected string
+	}{
+		{
+			i: []interface{}{123, "123", "blah", false},
+			expected: string([]byte{91, 10, 32, 32, 49, 50, 51, 44, 10, 32, 32, 34, 49, 50, 51, 34, 44, 10, 32, 32, 34, 98, 108, 97, 104, 34, 44, 10, 32, 32, 102, 97, 108, 115, 101, 10, 93}),
+		},
+		{
+			i: []interface{}{"1234.01", -34, []byte{1}},
+			expected: string([]byte{91, 10, 32, 32, 34, 49, 50, 51, 52, 46, 48, 49, 34, 44, 10, 32, 32, 45, 51, 52, 44, 10, 32, 32, 34, 65, 81, 61, 61, 34, 10, 93}),
+		},
+		{
+			i: []interface{}{byte(0), uint64(1), []string{"i", "f"}},
+			expected: string([]byte{91, 10, 32, 32, 48, 44, 10, 32, 32, 49, 44, 10, 32, 32, 91, 10, 32, 32, 32, 32, 34, 105, 34, 44, 10, 32, 32, 32, 32, 34, 102, 34, 10, 32, 32, 93, 10, 93}),
+		},
+	}
+
+	for i, tt := range tests {
+		t.Run(strconv.Itoa(i), func(t *testing.T) {
+			if Prettypi(tt.i) != tt.expected {
+				t.Error("return value of Prettyp does not match expected value")
+			}
+		})
+	}
+}
+
 
