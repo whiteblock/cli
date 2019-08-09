f5ca95564ddc98bd149a16e79f414e7c998f497e
n8wb <vanish172@gmail.com>
2019-08-09 14:56:07 -0400

    status

---
+++ go.mod
@@ -0,0 +1,22 @@
+module github.com/whiteblock/cli
+
+go 1.12
+
+require (
+	github.com/Whiteblock/go-prettyjson v0.0.0-20180920040306-f579f869bbfe
+	github.com/fatih/color v1.7.0 // indirect
+	github.com/gizak/termui/v3 v3.1.0
+	github.com/gorilla/rpc v1.2.0
+	github.com/gorilla/websocket v1.4.0
+	github.com/graarh/golang-socketio v0.0.0-20170510162725-2c44953b9b5f
+	github.com/kballard/go-shellquote v0.0.0-20180428030007-95032a82bc51 // indirect
+	github.com/mattn/go-colorable v0.1.2 // indirect
+	github.com/mattn/go-sqlite3 v1.11.0
+	github.com/sirupsen/logrus v1.4.2
+	github.com/spf13/cobra v0.0.5
+	github.com/spf13/viper v1.4.0
+	github.com/tmc/scp v0.0.0-20170824174625-f7b48647feef
+	golang.org/x/crypto v0.0.0-20190701094942-4def268fd1a4
+	golang.org/x/sync v0.0.0-20190423024810-112230192c58
+	golang.org/x/sys v0.0.0-20190726091711-fc99dfbffb4e
+)
---
+++ whiteblock/cmd/build/status.go
@@ -0,0 +1,29 @@
+package build
+
+import(
+	"fmt"
+	"github.com/whiteblock/cli/whiteblock/util"
+)
+
+type Status struct {
+	Error    map[string]interface{} `json:"error"`
+	Progress float64                `json:"progress"`
+	Stage    string                 `json:"stage"`
+	Frozen   bool                   `json:"frozen"`
+}
+
+func (status Status) Print() {
+	if status.Frozen {
+		fmt.Printf("\nBuild is currently frozen. Press Ctrl-\\ to drop into console. Run 'whiteblock build unfreeze' to resume. \r")
+	} else if status.Error != nil {
+		fmt.Println() //move to the next line
+		util.PrintErrorFatal(status.Error["what"])
+	} else if status.Progress == 0.0 {
+		fmt.Printf("Sending build context to Whiteblock\r")
+	} else if status.Progress == 100.0 {
+		fmt.Printf("\033[1m\033[K\033[31m%s\033[0m\t%f%% completed\r", "Build", status.Progress)
+		util.Print("\a")
+	} else {
+		fmt.Printf("\033[1m\033[K\033[31m%s\033[0m\t%f%% completed\r", status.Stage, status.Progress)
+	}
+}
