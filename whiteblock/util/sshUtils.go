8bcf62631632a5825a126cf20f60ae1726fea43f
n8wb <vanish172@gmail.com>
2019-08-18 13:39:50 +0200

    remove useless functions

--- whiteblock/util/sshUtils.go
+++ whiteblock/util/sshUtils.go
@@ -25,6 +25,7 @@ func NewSshClient(host string) (*SshClient, error) {
 	}
 	return out, nil
 }
+
 func (this SshClient) GetSession() (*ssh.Session, error) {
 	for _, client := range this.clients {
 		session, err := client.NewSession()
@@ -53,21 +54,6 @@ func (this SshClient) MultiRun(commands ...string) ([]string, error) {
 	return out, nil
 }
 
-/**
- * Speeds up remote execution by chaining commands together
- * @param  ...string    commands    The commands to execute
- * @return string                   The result of the execution
- */
-func (this SshClient) FastMultiRun(commands ...string) (string, error) {
-	cmd := ""
-	for i, command := range commands {
-		if i != 0 {
-			cmd += "&&"
-		}
-		cmd += command
-	}
-	return this.Run(cmd)
-}
 func (this SshClient) Run(command string) (string, error) {
 	session, err := this.GetSession()
 	defer session.Close()
@@ -78,45 +64,6 @@ func (this SshClient) Run(command string) (string, error) {
 
 	out, err := session.CombinedOutput(command)
 	return string(out), err
-}
-
-func (this SshClient) DockerExec(node int, command string) (string, error) {
-	return this.Run(fmt.Sprintf("docker exec whiteblock-node%d %s", node, command))
-}
-func (this SshClient) DockerExecd(node int, command string) (string, error) {
-	return this.Run(fmt.Sprintf("docker exec -d whiteblock-node%d %s", node, command))
-}
-func (this SshClient) DockerRead(node int, file string) (string, error) {
-	return this.Run(fmt.Sprintf("docker exec whiteblock-node%d cat %s", node, file))
-}
-func (this SshClient) DockerMultiExec(node int, commands []string) (string, error) {
-	merged_command := ""
-	for _, command := range commands {
-		if len(merged_command) != 0 {
-			merged_command += "&&"
-		}
-		merged_command += fmt.Sprintf("docker exec -d whiteblock-node%d %s", node, command)
-	}
-	return this.Run(merged_command)
-}
-
-/**
- * Copy a file over to a remote machine
- * @param  string   host    The IP address of the remote host
- * @param  string   src     The source path of the file
- * @param  string   dest    The destination path of the file
- */
-func (this SshClient) Scp(src string, dest string) error {
-	session, err := this.GetSession()
-	if err != nil {
-		return err
-	}
-	defer session.Close()
-	err = scp.CopyPath(src, dest, session)
-	if err != nil {
-		return err
-	}
-	return nil
 }
 
 func (this SshClient) Close() {
